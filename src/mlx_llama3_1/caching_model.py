d08426013f8d126d6e2cb976de93e4fa7ab4a914
kawamou <kwmkzsd@gmail.com>
2024-08-18 23:18:53 +0900

    caching_modelに切り出し

---
+++ src/mlx_llama3_1/caching_model.py
@@ -0,0 +1,25 @@
+import hashlib
+from typing import Dict, List, cast
+
+import mlx.core as mx
+from mlx_lm.models.llama import Model
+
+type Cache = Dict[str, mx.array]
+
+
+class CachingModel:
+    def __init__(self, model: Model):
+        self._model = model
+        self._cache: Cache = {}
+
+    def __call__(self, input_ids: mx.array) -> mx.array:
+        input_ids_str = ",".join(map(str, cast(List, input_ids.tolist())[0]))
+        key_ = hashlib.sha256(input_ids_str.encode())
+        key = key_.hexdigest()
+        if key not in self._cache:  # TODO キャッシュは後から計算するためなので正確にはこのif文は不要
+            logits = self._model(input_ids)
+            self._cache[key] = logits
+        return self._cache[key]
+
+    def clear(self):
+        self._cache = {}
